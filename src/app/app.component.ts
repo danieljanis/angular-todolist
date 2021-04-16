24cdb4ad4335eee7f2ecaf93516691645bb3ec13
Daniel Janis <danieljanis01@gmail.com>
2021-04-16 15:33:14 -0500

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Decorator, includes metadata for the component
+@Component({
+  selector: 'app-root', // Used in the HTML element (index.html)
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-todolist';
+}
